5c4ef4e744d1b9cb56c9c85fb98979e8b8354cf6
jaronoun <jaronoun@hotmail.com>
2020-04-30 01:47:55 +0200

    add game page

--- Angular(Front-end)/gameguide-app/src/app/pages/addgame/addgame.component.ts
+++ Angular(Front-end)/gameguide-app/src/app/pages/addgame/addgame.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from 'node_modules/materialize-css';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
+import * as $ from 'node_modules/jquery';
 
 @Component({
   selector: 'app-addgame',
@@ -10,6 +13,24 @@ export class AddgameComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    M.AutoInit();
+
+    window.onload = function() {
+      var elems  = document.querySelectorAll("input[type=range]");
+      M.Range.init(elems);
+
+    };
+
+    
   }
 
 }
+/**
+ * title
+ * image-bestand
+ * desc
+ * -------
+ * players
+ * time
+ * steps
+*/
